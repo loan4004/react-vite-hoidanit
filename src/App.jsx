64c74df483587c91df05c4cc9f79c5740d6fcc78
Nguyen Thi Kieu Loan <loantoet1234@gmail.com>
2026-04-02 16:34:44 +0700

    video 28

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 // () => { }
+// component = html + css + javascript
+
+const MyComponent = () => {
+  return (
+    <div> eric & hoidanit update </div> // html //tag
+  );
+}
+
 const App = () => {
   const [count, setCount] = useState(0)
 
-  // function myFunction() {
-  //   console.log(">>> run my function")
-  // }
 
-  const myFunction = () => {
-    console.log(">>> run my arrow function")
-  }
-  myFunction();
 
   return (
     <>
@@ -26,7 +27,8 @@ const App = () => {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hello world & hoidanitgit remote set-url origin</h1>
+      <h1>Hello world & hoidanitgit </h1>
+      <MyComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
