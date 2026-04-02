9a9fd1e4ee6ec51550a8fe5300279b60283afe93
Nguyen Thi Kieu Loan <loantoet1234@gmail.com>
2026-04-02 18:21:57 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,10 +1,19 @@
 // () => { }
 // component = html + css + javascript
+//fragment manh vo
+//JSX : 1 parent
 
+import './style.css';
 
 const MyComponent = () => {
     return (
-        <div> eric & hoidanit update </div> // html //tag
+        <>
+            <div> eric & hoidanit update </div>
+            <div className="child"
+                style={{ borderRadius: "10px" }}
+            >child</div>
+
+        </>
     );
 }
 
