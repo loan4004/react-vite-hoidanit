233aeb24ef2112db0cf7e741d6a805c9cb733258
Nguyen Thi Kieu Loan <loantoet1234@gmail.com>
2026-04-06 17:44:10 +0700

    video 33

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,11 @@
+
+const TodoData = () => {
+    return (
+        <div className='todo-data'>
+            <div>Learning react</div>
+            <div>Watching Youtube</div>
+        </div>
+    )
+}
+
+export default TodoData;
