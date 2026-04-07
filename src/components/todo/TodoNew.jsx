d578e92111d403fa764d6e33fa328e559a4a483d
Nguyen Thi Kieu Loan <loantoet1234@gmail.com>
2026-04-07 23:32:33 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -3,12 +3,25 @@ const TodoNew = (props) => {
     console.log(">>> check point: ", props)//khi ban hong biet 1 bien so gia tri ntn thi dung console.log
     const { addNewTodo } = props;
 
-    // addNewTodo("eric")
+    // addNewTodo("eric") //fire : khai hoar
+
+    const handleClick = () => {
+        alert("click me")
+    }
+
+    const handleOnChange = (name) => {
+        console.log(">>> handleOnChange", name)
+    }
 
     return (
         <div className='todo-new'>
-            <input type="text" />
-            <button>Add</button>
+            <input type="text"
+                onChange={(event) => handleOnChange(event.target.value)}
+            />
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }
